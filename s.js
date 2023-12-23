e92dcbfce3132b2f925165cc864e7f2a8809d730
Aniss Chalah <39713625+ACCISA@users.noreply.github.com>
2023-12-22 22:21:23 -0500

    Update s.js

--- s.js
+++ s.js
@@ -2,10 +2,8 @@ console.log("started")
 fetch('/login.php', {
   method: 'POST',
   headers: {
-    'Host': 'challenges.montrehack.ca:8580',
     'Content-Length': '24',
     'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
     'Origin': 'http://challenges.montrehack.ca:8580',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -13,11 +11,11 @@ fetch('/login.php', {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'Cookie': 'PHPSESSID=73f98d1e9d58bd3f003cbca37683836f',
-    'Connection': 'close',
     'User-Agent': 'hello'
   },
   body: 'username=rome&password=somerandox',
-  mode: 'cors'
+  mode: 'cors',
+  credentials: 'include'
 });
 console.log("worked")
 
