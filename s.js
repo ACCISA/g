0e57311d2b42c4b88195f3bce4c5d3e7cd19a940
Aniss Chalah <39713625+ACCISA@users.noreply.github.com>
2023-12-22 21:46:21 -0500

    Update s.js

--- s.js
+++ s.js
@@ -2,9 +2,8 @@ alert("trying to go to login")
 fetch('/login.php', {
   method: 'POST',
   headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
     'User-Agent': 'hello'
   },
   body: 'username=rome&password=x'
 });
-console("worked")
+console.log("worked")
